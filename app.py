b9d7c61cb2a6f1479d459ce443684579d6e51025
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-07 23:46:06 +0530

    hello world

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from flask import Flask, render_template, url_for
 from website import start_app
 app = start_app()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # starting our app
 if __name__ == '__main__':
--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def start_app():
     app = Flask(__name__)
