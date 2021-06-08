57ae0431865485237364e142b1c1c137340081b5
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-09 02:48:34 +0530

    updated bootstrap

--- website/dbtrial.py
+++ website/dbtrial.py
@@ -1,8 +1,40 @@
-# # import pyodbc
-# from sqlalchemy import create_engine
+# mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:ansh-db-server.database.windows.net,1433;Database=ansh-db;Uid=ansh-db-server-admin;Pwd=mydbpassword@001;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+import pyodbc
+import textwrap
 
-# conn_str = 'mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:ansh-db-server.database.windows.net,1433;Database=ansh-db;Uid=ansh-db-server-admin;Pwd=mydbpassword@001;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-# engine_azure = create_engine(conn_str,echo=True)
+#  Connection Parameters
+driver = "{ODBC Driver 17 for SQL Server}"
+server_name = "ansh-db-server"
+database_name = "ansh-db"
+server =  f'{server_name}.database.windows.net,1433'
+username = 'ansh-db-server-admin'
+password = 'mydbpassword@001'
 
-# print('connection is ok')
-# print(engine_azure.table_names())
+# Connection String
+conn_url = f"Driver={driver};Server=tcp:{server};Database={database_name};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+
+#Connection STARTED
+cnxn = pyodbc.connect(conn_url)
+print("Connection OPEN")
+
+
+# Run Querry
+
+
+cursor = cnxn.cursor()
+cursor.execute("INSERT INTO UserData VALUES (0003, '19bcs4085@cuchd.in', 'Lakshay', 'Chhabra');") 
+cursor.commit()
+# cursor.update("INSERT INTO UserData VALUES (0003, '19bcs4085@cuchd.in', 'Lakshay', 'Chhabra');") 
+# cursor.execute("SELECT * FROM UserData") 
+# row = cursor.fetchone() 
+# while row:
+#     print(row)
+#     row = cursor.fetchone()
+
+
+
+
+
+# Connection CLOSED
+cnxn.close()
+print("Connection CLOSE")
