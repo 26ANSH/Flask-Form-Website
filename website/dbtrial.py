3552ea5f4de91fdd52e3d21158f078d5346b632f
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-08 21:03:44 +0530

    Update dbtrial.py

--- website/dbtrial.py
+++ website/dbtrial.py
@@ -1,8 +1,8 @@
-# import pyodbc
-from sqlalchemy import create_engine
+# # import pyodbc
+# from sqlalchemy import create_engine
 
-conn_str = 'mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:ansh-db-server.database.windows.net,1433;Database=ansh-db;Uid=ansh-db-server-admin;Pwd=mydbpassword@001;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-engine_azure = create_engine(conn_str,echo=True)
+# conn_str = 'mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:ansh-db-server.database.windows.net,1433;Database=ansh-db;Uid=ansh-db-server-admin;Pwd=mydbpassword@001;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+# engine_azure = create_engine(conn_str,echo=True)
 
-print('connection is ok')
-print(engine_azure.table_names())
+# print('connection is ok')
+# print(engine_azure.table_names())
