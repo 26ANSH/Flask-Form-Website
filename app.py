f1d3482f86f3d1e223a7c32fa9edff44633990c8
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-08 21:08:08 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
 from website import start_app
 app = start_app()
 
-# starting our app
+# starting our app - Development
 if __name__ == '__main__':
     app.run(debug=True)
+
+# starting our app - Deployment / Production
+# if __name__ == '__main__':
+#     app.run()
