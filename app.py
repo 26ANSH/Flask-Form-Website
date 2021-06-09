96fc354e1bbc09a0eaa173e9e5b2c185e7d55c04
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-10 01:39:08 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app = start_app()
 
 # starting our app - Development
 if __name__ == '__main__':
+    app.secret_key='helloworld'
     app.run(debug=True)
 
 # starting our app - Deployment / Production
