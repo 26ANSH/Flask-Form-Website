b80848319c1f701ce1f746e53d48c5da76b0dac6
26ANSH <anshvidyabhanu8@gmail.com>
2021-06-11 23:41:27 +0530

    ok

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,46 @@
 from flask import Blueprint, render_template, request, session
-from flask_mail import Message, Mail
+from flask_mail import Message
+
 from . import conn_url, mail
 from .views import WP_MSG, MSG_URL
+
 import pyodbc
+from threading import Thread
 auth = Blueprint('auth', __name__)
 cnxn = pyodbc.connect(conn_url)
 cursor = cnxn.cursor()
 
+def send_ok_mail(rec):
+    msg = Message(subject='Web Development with Python',
+                                sender='ansh.vidyabhanu@studentambassadors.com',
+                                recipients=[rec],)
+    msg.body = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            padding: 4vh; 
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body >
+    <h1>Congratulations you have sucessfully Registered</h1>
+    <h3>
+        Thank you For Registering for this event
+    <br>
+    Event Link : 
+    <br>
+    Details : 
+    </h3>
+</body>
+</html>'''
+    msg.html = msg.body 
+    msg.msgId = msg.msgId.split('@')[0] + 'studentambassadors.com'
+    mail.send(msg)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -34,12 +68,8 @@ def register():
                     session["registered"] = 'done'
 
                     # SEnd Mails
-                    msg = Message(subject='Web Development with Python',
-                                sender='ansh.vidyabhanu@studentambassadors.com',
-                                recipients=[emailid],
-                                body='First Flask-mail msg')
-                    msg.msgId = msg.msgId.split('@')[0] + 'studentambassadors.com'
-                    mail.send(msg)
+                    sending = Thread(target=send_ok_mail, args=[emailid])
+                    sending.start()
 
                     # print("session ser",session["registered"])
                     cursor.execute(f"INSERT INTO UserData VALUES ('{emailid}', '{fname}', '{lname}', '{country}');") 
